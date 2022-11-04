5263a0708c824bd67422f2a5b5584548f6b21342
Paul Jacobson <20469645+pajacobson@users.noreply.github.com>
2022-11-04 21:30:49 +1100

    Black reformat of function parameters

--- CreateCeedlingModule.py
+++ CreateCeedlingModule.py
@@ -2,16 +2,22 @@ import functools
 import sublime
 import sublime_plugin
 
-class CreateCeedlingModuleCommand(sublime_plugin.WindowCommand):
 
-	def run(self):
-		window = self.window
-		view = self.window.active_view()
-		current_file_path = view.file_name()
-		window.show_input_panel("Enter module path", current_file_path, functools.partial(self.onDone, view), None, None)
-	
-	def onDone(self, view, text):
-		window = view.window()
-		sublime.status_message("Creating module: " + text)
-		window.run_command("rake", { "tasks": [ "module:create[" + text + "]" ] } )
-		sublime.status_message("Created module: " + text)
+class CreateCeedlingModuleCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        window = self.window
+        view = self.window.active_view()
+        current_file_path = view.file_name()
+        window.show_input_panel(
+            "Enter module path",
+            current_file_path,
+            functools.partial(self.onDone, view),
+            None,
+            None,
+        )
+
+    def onDone(self, view, text):
+        window = view.window()
+        sublime.status_message("Creating module: " + text)
+        window.run_command("rake", {"tasks": ["module:create[" + text + "]"]})
+        sublime.status_message("Created module: " + text)
