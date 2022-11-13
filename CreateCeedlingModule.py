c91daa9cdd1a58d429f75344afe9d68f9f470460
Paul Jacobson <20469645+pajacobson@users.noreply.github.com>
2022-11-13 17:41:52 +1100

    Adding support for stub creation. stubs are currently broken in Ceedling ruby code.

--- CreateCeedlingModule.py
+++ CreateCeedlingModule.py
@@ -5,20 +5,21 @@ import sublime_plugin
 
 
 class CreateCeedlingModuleCommand(sublime_plugin.WindowCommand):
-    def run(self):
+    def run(self, module=""):
+
         window = self.window
         view = self.window.active_view()
 
         window.show_input_panel(
-            "Enter new module name",
+            f"Enter new {module} name",
             "",
-            functools.partial(self.onDone, view),
+            functools.partial(self.onDone, view, module),
             None,
             None,
         )
 
-    def onDone(self, view, text):
+    def onDone(self, view, module, text):
         window = view.window()
-        window.status_message("Creating module: %s" % text)
-        window.run_command("ceedling", {"tasks": ["module:create[%s]" % text]})
-        window.status_message("Created module: %s" % text)
+        window.status_message(f"Creating {module}: {text}")
+        window.run_command("ceedling", {"tasks": [f"module:{module}[{text}]"]})
+        window.status_message(f"Created {module}: {text}")
