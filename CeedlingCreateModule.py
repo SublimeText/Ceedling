aaca5ae3a7932778a014f5894cc844bd86632e62
Paul Jacobson <20469645+pajacobson@users.noreply.github.com>
2022-11-15 18:04:38 +1100

    Update module names

---
+++ CeedlingCreateModule.py
@@ -0,0 +1,25 @@
+import functools
+
+import sublime
+import sublime_plugin
+
+
+class CeedlingCreateModuleCommand(sublime_plugin.WindowCommand):
+    def run(self, module=""):
+
+        window = self.window
+        view = self.window.active_view()
+
+        window.show_input_panel(
+            f"Enter new {module} name",
+            "",
+            functools.partial(self.onDone, view, module),
+            None,
+            None,
+        )
+
+    def onDone(self, view, module, text):
+        window = view.window()
+        window.status_message(f"Creating {module}: {text}")
+        window.run_command("ceedling", {"tasks": [f"module:{module}[{text}]"]})
+        window.status_message(f"Created {module}: {text}")
