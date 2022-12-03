0ea0b908cbcb5a727f6104c3e6ee61222baa8054
Paul Jacobson <20469645+pajacobson@users.noreply.github.com>
2022-12-03 13:48:25 +1100

    Split path functions into a new class

--- CeedlingOpenFile.py
+++ CeedlingOpenFile.py
@@ -1,10 +1,94 @@
 import os
 import re
+import itertools
 
 import sublime_plugin
 
 from .CeedlingSettings import CeedlingProjectSettings
 from . import glob2
+
+
+class CeedlingPathBuilder:
+    def __init__(self, settings):
+        self.conf = settings
+
+    def split_name(self, filename):
+        """Return dict of file name components."""
+        result = re.search(
+            r"(?P<prefix>{})?".format(self.conf.test_file_prefix)
+            + r"(?P<base>.+?)\."
+            + r"(?P<ext>{}|{})$".format(
+                self.conf.source_ext, self.conf.header_ext
+            ),
+            filename,
+        )
+        return {} if result is None else result.groupdict()
+
+    def build_path(self, option, base):
+        # todo: Check this assumption holds when env is set
+        working_dir = self.conf.working_dir
+        ext = self.conf.source_ext
+
+        if working_dir is None:
+            raise IOError("missing path")
+
+        if option == "test":
+            included = self.conf.test
+            excluded = self.conf.test_excl
+            base = "".join((self.conf.test_file_prefix, base))
+
+        elif option == "source":
+            included = self.conf.source
+            excluded = self.conf.source_excl
+
+        else:
+            included = self.conf.includes
+            excluded = self.conf.includes_excl
+            ext = self.conf.header_ext
+
+        # Build list of matching files within configured directories
+        file_list = list(
+            self._glob_search(included, working_dir, base, ext)
+            - self._glob_search(excluded, working_dir, base, ext)
+        )
+
+        if len(file_list) == 0:
+            self.window.status_message("No matching file")
+            return None
+
+        elif len(file_list) > 1:
+            self.window.status_message("Ceedling: Multiple matching files.")
+
+            print("Duplicate matches found:")
+
+            for dup in file_list:
+                print(dup)
+
+        return file_list[0]
+
+    def _glob_search(self, pattern, path, base, ext):
+        """Return set of files matching glob path.
+
+        Ceedling project.yml uses globstar `**` pattern.
+        This is not supported by Python before v3.5.
+        `glob2` module backports this functionality to earlier Python versions
+        and is used to maintain Sublime Text 3 support.
+        """
+        return set(
+            itertools.chain.from_iterable(
+                glob2.iglob(
+                    os.path.abspath(
+                        os.path.join(
+                            path,
+                            os.path.normpath(p),
+                            ".".join((base, ext)),
+                        )
+                    ),
+                    recursive=True,
+                )
+                for p in pattern
+            )
+        )
 
 
 class CeedlingOpenFileCommand(sublime_plugin.WindowCommand):
@@ -13,11 +97,14 @@ class CeedlingOpenFileCommand(sublime_plugin.WindowCommand):
         self.views = []
         window = self.window
 
+        variables = self.window.extract_variables()
+
         if not self.window.active_view():
             return
 
         try:
             self.conf = CeedlingProjectSettings(self.window)
+            self.pathbuilder = CeedlingPathBuilder(self.conf)
 
         except OSError as e:
             self.window.status_message("Ceedling: %s" % e)
@@ -26,34 +113,21 @@ class CeedlingOpenFileCommand(sublime_plugin.WindowCommand):
         if option == "config":
             return self._open_file(self.conf.project_yml)
 
-        # Handle files within the project structure
-        current_file_path = self.window.active_view().file_name()
-
-        if current_file_path is None:
-            return
-
-        _, filename = os.path.split(current_file_path)
-
-        filename = re.search(
-            r"(?P<prefix>{})?".format(self.conf.test_file_prefix)
-            + r"(?P<base>.+?)\."
-            + r"(?P<ext>{}|{})$".format(
-                self.conf.source_ext, self.conf.header_ext
-            ),
-            filename,
-        )
+        # Extract file name components
+        filename = self.pathbuilder.split_name(variables.get("file_name"))
 
         if filename is None:
             self.window.status_message("Ceedling switching: unsupported file")
             return
 
-        base_name = filename.group("base")
+        # Filename less extension and test prefix
+        base_name = filename.get("base")
 
         try:
             if option == "next":
-                if filename.group("prefix"):
+                if filename.get("prefix") == self.conf.test_file_prefix:
                     option = "source"
-                elif filename.group("ext") == "c":
+                elif filename.get("ext") == self.conf.source_ext:
                     option = "header"
                 else:
                     option = "test"
@@ -68,80 +142,22 @@ class CeedlingOpenFileCommand(sublime_plugin.WindowCommand):
                     },
                 )
 
-                self._open_file(self._path_build("test", base_name), 0)
-                self._open_file(self._path_build("header", base_name), 1),
-                self._open_file(self._path_build("source", base_name), 1),
+                self._open_file(
+                    self.pathbuilder.build_path("test", base_name), 0
+                )
+                self._open_file(
+                    self.pathbuilder.build_path("header", base_name), 1
+                ),
+                self._open_file(
+                    self.pathbuilder.build_path("source", base_name), 1
+                ),
 
             else:
-                self._open_file(self._path_build(option, base_name))
+                self._open_file(self.pathbuilder.build_path(option, base_name))
 
         except IOError as e:
             self.window.status_message("Ceedling: {}".format(e))
             return
-
-    def _path_build(self, option, base):
-        # todo: Check this assumption holds when env is set
-        ppath = self.conf.working_dir
-        ext = self.conf.source_ext
-
-        if ppath is None:
-            raise IOError("missing path")
-
-        if option == "test":
-            gpath = self.conf.test
-            xpath = self.conf.test_excl
-
-            base = "".join((self.conf.test_file_prefix, base))
-
-        elif option == "source":
-            gpath = self.conf.source
-            xpath = self.conf.source_excl
-
-        else:
-            gpath = self.conf.includes
-            xpath = self.conf.includes_excl
-            ext = self.conf.header_ext
-
-        # build list of files based on project.yml glob paths
-        incl = self._glob_search(gpath, ppath, base, ext)
-        excl = self._glob_search(xpath, ppath, base, ext)
-
-        # remove files from excluded directories from results
-        incl = list(set(incl) - set(excl))
-
-        if len(incl) == 0:
-            raise IOError("No matching file")
-
-        elif len(incl) > 1:
-            self.window.status_message("Ceedling: Multiple matching files.")
-
-            print("Duplicate matches found:")
-
-            for m in incl:
-                print(m)
-
-        return incl[0]
-
-    def _glob_search(self, pattern, path, base, ext):
-        """Return list of files matching glob path.
-
-
-        Ceedling project.yml uses globstar `**` pattern.
-        This is not supported by Python before v3.5.
-        `glob2` module backports this functionality to earlier Python versions
-        and is used to maintain Sublime Text 3 support.
-        """
-        res = []
-        for glob_pat in pattern:
-
-            glob_pat = os.path.normpath(glob_pat)
-
-            p = os.path.abspath(
-                os.path.join(path, glob_pat, ".".join((base, ext)))
-            )
-            res.extend(glob2.glob(p, recursive=True))
-
-        return res
 
     def _open_file(self, file_path, auto_set_view=-1):
         file_view = self.window.open_file(file_path)
